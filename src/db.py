74e9c3f229272e0b0cf0cd0d1c628bb79923c499
Ris <temich04om@mail.ru>
2025-06-06 18:39:17 +0600

    feat: sql db connect

---
+++ src/db.py
@@ -0,0 +1,13 @@
+from sqlmodel import create_engine, Session, SQLModel
+from src.core.config import settings
+
+DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
